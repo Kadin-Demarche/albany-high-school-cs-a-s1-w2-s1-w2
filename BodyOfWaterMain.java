1ce95cbf4aea8c56efb40e3ed4fc98c736d28f2b
Kadin DeMarche <107492686+Kadin-Demarche@users.noreply.github.com>
2025-08-22 14:18:33 -0700

    push

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,21 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("lakey", 10.5, 5.5, false);
+        BodyOfWater b2 = new BodyOfWater("lakey jr", 5, 5, true);
+        b.printState();
+        b1.printState();
     }
 }
+
+
+
+
+
+
+
+
+
+// Make 2 instances of the BodyOfWater using each constructor
+// Print out the state of each object
+// On one instance call the setName method 
+// Print out the state again showing the change from above
